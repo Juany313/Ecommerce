54a75487238fee28aeaa301e892d28af0408dd7e
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-08-24 22:26:55 -0300

    Actualiza configuración de Vite para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://Juany313.github.io/Ecommerce'
+  base: '/Ecommerce/',
+  build: {
+    outDir: 'dist',  // Esto asegura que los archivos se generen en el directorio `dist`
+  },
 })
