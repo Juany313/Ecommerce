5c836d9f026290b67d7ab1f4cb85eae4e86ab556
Juan Javier Geringer <juanjaviergeringer@gmail.com>
2024-08-25 11:27:51 -0300

    correccion en import

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Image1 from "../../assets/Hero/hombre-comprando.png";
 import Image2 from "../../assets/Hero/woman-shop.png";
-import Image4 from "../../assets/Hero/shopping.jpg";
 import Image3 from "../../assets/Hero/sale.png";
 import Slider from "react-slick";
 
